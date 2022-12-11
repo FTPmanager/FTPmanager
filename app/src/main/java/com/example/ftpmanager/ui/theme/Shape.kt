441c2738f23a2982fc491c1c8e8dc328e0642415
Szewczyk Bartosz <260472@student.pwr.edu.pl>
2022-12-11 18:59:06 +0100

    Theme and shape

---
+++ app/src/main/java/com/example/ftpmanager/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.example.ftpmanager.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+
+val Shape = Shapes(
+    small = RoundedCornerShape(50),
+    medium = RoundedCornerShape(50),
+    large = RoundedCornerShape(50)
+)
