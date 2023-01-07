aa505e183863899330d2d77d3ffb43ccd8bbf7af
Szewczyk Bartosz <260472@student.pwr.edu.pl>
2023-01-07 15:02:58 +0100

    test button for infrastructure functions

--- app/src/main/java/com/example/ftpmanager/ui/components/complex_components/FileManagerMenu.kt
+++ app/src/main/java/com/example/ftpmanager/ui/components/complex_components/FileManagerMenu.kt
@@ -1,14 +1,21 @@
 package com.example.ftpmanager.ui.components.complex_components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.ftpmanager.R
+import com.example.ftpmanager.ui.components.basic_components.TextButton
 
 @Composable
 fun FileManagerMenu(
     modifier: Modifier = Modifier
 ) {
     Text(text = stringResource(R.string.wip))
+    TextButton(
+        text = R.string.test_button,
+        onClick = { /*TODO*/ },
+        modifier = modifier.fillMaxWidth()
+    )
 }
